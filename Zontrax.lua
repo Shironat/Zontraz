a38a44ec2e0fb156307dda3a2904bfc7cb2cb821
Shiro NAT <manoelrodriguesdev@gmail.com>
2026-01-31 17:16:15 -0300

    Atualizar o Zontrax.lua

--- Zontrax.lua
+++ Zontrax.lua
@@ -1,14 +1,31 @@
-local Zonix = loadstring(game:HttpGet("https://hub.zon.su/zonix-ui.lua"))()
+-- Main.lua (LocalScript)
 
--- Cria a janela
+print("Main iniciou")
+
+local Zonix = loadstring(game:HttpGet("https://hub.zon.su/zonix-ui.lua"))()
+print("UI carregada")
+
 local Window = Zonix:CreateWindow({
-    Name = "ZonixHub",
-    Icons = {}
+    Name = "Shiro Hub",
+    Size = UDim2.new(0, 500, 0, 400),
+    Theme = "Dark"
 })
+
+print("Janela criada")
 
 local Tabs = {}
 Tabs.Main = Window:CreateTab("Main")
 Tabs.Farm = Window:CreateTab("Farm")
 
+print("Tabs criadas")
+
+-- Ajuste o caminho se necessário
 local TsunamiEscape = require(script.TsunamiEscape)
+local FarmCollectMoney = require(script.FarmCollectMoney)
+
+print("Módulos carregados")
+
 TsunamiEscape:CreateToggle(Tabs.Main)
+FarmCollectMoney:CreateToggle(Tabs.Farm)
+
+print("Toggles criados")
