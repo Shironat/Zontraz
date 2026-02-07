5cfa9d9642c936d072b4dfd67f142e8e73e12529
Shiro NAT <manoelrodriguesdev@gmail.com>
2026-02-07 08:35:29 -0300

    Atualizar o Zontrax.lua

--- Zontrax.lua
+++ Zontrax.lua
@@ -18,12 +18,20 @@ local Tabs = {}
 
 Tabs.Autofarm  = Window:Tab({Name = "Auto farm"})
 Tabs.Teleport  = Window:Tab({Name = "Teleport"})
+Tabs.Player    = Window:Tab({Name = "Player"})
+Tabs.Misc      = Window:Tab({Name = "Misc"})
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Shironat/Zontraz/main/Tabs/Autofarm.lua"))()(Tabs.Autofarm)
 print("Autofarm: 200[OK]")
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Shironat/Zontraz/main/Tabs/Teleport.lua"))()(Tabs.Teleport)
 print("Teleport: 200[OK]")
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Shironat/Zontraz/main/Tabs/Player.lua"))()(Tabs.Player)
+print("Player: 200[OK]")
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Shironat/Zontraz/main/Tabs/Misc.lua"))()(Tabs.Misc)
+print("Misc: 200[OK]")
 
 
 print("Zonix carregado!")
